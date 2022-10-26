d16a9903e913efbe6b264073362649cce857b4ba
Yiyi <eric9991517@gmail.com>
2022-10-26 23:57:24 +0800

    uodate content

--- src/components/work.vue
+++ src/components/work.vue
@@ -12,6 +12,7 @@
             <div class="grid-content ep-bg-purple" />
             <h2>緯創軟體 Wistron ITS</h2>
             <h4>Software Engineer</h4>
+            <p>協助國泰人壽進行後台維護及開發新功能<br>使用Java、Springboot、iReport</p>
           </el-col>
           <el-col :span="6" :xs="24">
             <div class="grid-content ep-bg-purple" />
@@ -29,6 +30,7 @@
             <div class="grid-content ep-bg-purple" />
             <h2>成霖實業 GLOBE UNION</h2>
             <h4>Software Engineer</h4>
+            <p>現有BPM系統維護及開發新程式<br>客製化表單及簽核流程<br>客製化報表<br>使用Java、Springboot、Vue2、FineReport</p>
           </el-col>
           <el-col :span="6" :xs="24">
             <div class="grid-content ep-bg-purple" />
@@ -46,7 +48,7 @@
             <div class="grid-content ep-bg-purple" />
             <h2>八旺實業 BarWand Corp.</h2>
             <h4>Technical Support Engineer</h4>
-            <br />
+            <p>精通各廠牌標籤積及Barcode Reader維修保養<br>客製標籤機程式(VB6 & VB.NET & C#)<br>客製化攜帶型PDA進出庫掃碼程式(WinCE & C)</p>
           </el-col>
           <el-col :span="6" :xs="24">
             <div class="grid-content ep-bg-purple" />
@@ -64,6 +66,9 @@
             <div class="grid-content ep-bg-purple" />
             <h2>寶成集團 POU CHEN GROUP</h2>
             <h4>Software Engineer</h4>
+            <P>將現有自動化產生3D鞋模程式從VB6改寫成VB.NET
+              <br>將PowerSHAPE做二次開發公司內部專用tools
+            </P>
           </el-col>
           <el-col :span="4" :xs="24">
             <div class="grid-content ep-bg-purple" />
