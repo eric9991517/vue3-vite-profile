3b04e9487bd9f5de4fd6e2a7e95c106498a0ac41
Yiyi <eric9991517@gmail.com>
2022-10-26 14:54:39 +0800

    調整css

--- src/components/info.vue
+++ src/components/info.vue
@@ -34,7 +34,7 @@
             </div>
             <span class="info-text-content"><i class="fa-solid fa-location-dot fa-xl" style="color:red"></i> <b>Taipei , TW</b></span>
             <br>  
-            <div class="info-text-content">
+            <div class="info-text-content" style="padding: 15px;">
                 <p>擁有6年後端經驗，喜歡在工作過程中自學程式研究新穎技術，目前在 <b>緯創軟體</b> 擔任後端工程師。期許自己持續精進技術實力並結合過往經驗，協助公司解決問題。</p>
                 <!-- <p>擁有6年後端經驗，喜歡在工作過程中自學程式研究新穎技術<br>目前在 <b>緯創軟體</b> 擔任後端工程師，期許自己持續精進技術實<br>力並結合過往經驗，協助公司解決問題。</p> -->
             </div>
@@ -57,13 +57,13 @@
     font-weight:bold;
     text-align:left;
     margin-left:-20px;
-  
+    padding: 15px
 }
 .info-text-content{
     width:360px;
     text-align:left;
     margin-left: -20px;
-    /* word-wrap:break-all; */
+    padding: 15px
 }
 .icon-span{
     padding: 3px;
